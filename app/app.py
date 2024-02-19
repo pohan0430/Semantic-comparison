df43b4f72e4b660d0d0babbe1ffdbeb8338a6a67
tooooooot <linyugung@gmail.com>
2024-02-19 17:22:50 +0800

    feat: add simple app and environment

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def process():
+    data = request.get_json()
+    input_string = data.get('input_string', '')
+    
+    result = process(input_string)
+    
+    return jsonify({'result': result})
+
+def process(input_string):
+    return input_string
+
+if __name__ == '__main__':
+    app.run(debug=True)
