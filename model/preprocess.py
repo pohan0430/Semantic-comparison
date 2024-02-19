4788f92b7fda7a55c4931c210af35dfdc538eb6b
pohan <pohan0430@gmail.com>
2024-02-20 00:06:30 +0800

    Add specific files to model folder

---
+++ model/preprocess.py
@@ -0,0 +1,31 @@
+import csv
+import torch
+from transformers import BertTokenizer, BertModel
+
+# Load the tokenizer and model
+tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
+model = BertModel.from_pretrained("bert-base-chinese")
+
+filename = "semantic_tags.tsv"
+titles = []
+
+with open(filename, "r", encoding="utf-8") as file:
+    reader = csv.DictReader(file, delimiter="\t")
+    for row in reader:
+        titles.append(row["title"])
+
+# Generate vector for each title
+title_to_vector = {}
+for title in titles:
+    encoded_input = tokenizer(title, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**encoded_input)
+    title_embedding = outputs.last_hidden_state[:, 0, :]
+    title_to_vector[title] = title_embedding
+print(title_to_vector)
+# Save the dictionary
+torch.save(title_to_vector, "title_to_vector.pt")
+
+print(
+    f"Saved the title to vector mappings for {len(titles)} titles in 'title_to_vector.pt'."
+)
---
+++ model/semantic_comparison.py
@@ -0,0 +1,34 @@
+import torch
+from transformers import BertTokenizer, BertModel
+from sklearn.metrics.pairwise import cosine_similarity
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
+model = BertModel.from_pretrained("bert-base-chinese")
+text = "幫找一些便宜美食的文章"
+encoded_input = tokenizer(text, return_tensors="pt")
+
+# Generate embeddings for the input text
+with torch.no_grad():
+    outputs = model(**encoded_input)
+sentence_embedding = outputs.last_hidden_state[:, 0, :].numpy()
+
+# Load the title to vector mappings
+title_to_vector = torch.load("title_to_vector.pt")
+titles = list(title_to_vector.keys())
+embeddings = torch.stack(list(title_to_vector.values())).squeeze(1).numpy()
+
+# Calculate cosine similarity between the input text and each title
+similarities = cosine_similarity(sentence_embedding, embeddings)
+
+# Display similarity scores
+for title, similarity in zip(titles, similarities[0]):
+    print(f"Similarity for title '{title}': {similarity}")
+
+
+# Find the most similar title
+most_similar_idx = similarities.argmax()
+most_similar_title = titles[most_similar_idx]
+most_similar_score = similarities[0, most_similar_idx]
+print(
+    f"Most similar title: '{most_similar_title}', Similarity score: {most_similar_score}"
+)
