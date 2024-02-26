f60ac936dc7669375196362298d25b362490bc76
pohan <pohan0430@gmail.com>
2024-02-26 14:28:36 +0800

    modifying codes in preprocess.py and semantic_comparison.py

--- model/preprocess.py
+++ model/preprocess.py
@@ -1,27 +1,33 @@
 import csv
 import torch
 from transformers import BertTokenizer, BertModel
+from tqdm import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the tokenizer and model
 tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
-model = BertModel.from_pretrained("bert-base-chinese")
+model = BertModel.from_pretrained("bert-base-chinese").to(device)
 
-filename = "semantic_tag_1500.tsv"
-titles = []
+filename = "semantic_tag_2023_7_1-2024_2_20.tsv"
+titles_and_urls = []
 
 with open(filename, "r", encoding="utf-8") as file:
     reader = csv.DictReader(file, delimiter="\t")
-    for row in reader:
-        titles.append(row["title"])
+    for row in tqdm(reader, desc="Reading file"):
+        titles_and_urls.append((row["title"], row["url"]))
 
-batch_size = 10
-title_to_vector = {}
+batch_size = 100
+title_url_to_vector = {}
 
-for i in range(0, len(titles), batch_size):
-    batch_titles = titles[i : i + batch_size]
+for i in tqdm(
+    range(0, len(titles_and_urls), batch_size), desc="Processing titles and URLs"
+):
+    batch = titles_and_urls[i : i + batch_size]
+    batch_titles = [title for title, url in batch]
     encoded_input = tokenizer(
         batch_titles, return_tensors="pt", padding=True, truncation=True, max_length=128
-    )
+    ).to(device)
     attention_mask = encoded_input["attention_mask"]
 
     with torch.no_grad():
@@ -35,12 +41,12 @@ for i in range(0, len(titles), batch_size):
     sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
     mean_embeddings = sum_embeddings / sum_mask
 
-    for title, embedding in zip(batch_titles, mean_embeddings):
-        title_to_vector[title] = embedding
+    for (title, url), embedding in zip(batch, mean_embeddings):
+        title_url_to_vector[f"{title} | {url}"] = embedding.cpu().numpy()
 
 # Save the dictionary
-torch.save(title_to_vector, "title_to_vector_1500.pt")
+torch.save(title_url_to_vector, "title_url_to_vector_2023_7_1-2024_2_20.pt")
 
 print(
-    f"Saved the title to vector mappings for {len(titles)} titles in 'title_to_vector_1500.pt'."
+    f"Saved the title and URL to vector mappings for {len(titles_and_urls)} items in 'title_url_to_vector.pt'."
 )
--- model/semantic_comparison.py
+++ model/semantic_comparison.py
@@ -5,26 +5,23 @@ from sklearn.metrics.pairwise import cosine_similarity
 import logging
 from datetime import datetime
 
-log_dir = "log"
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
 
-# Set logger
-current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-log_filename = f"similarity_{current_time}.log"
-logging.basicConfig(
-    filename=os.path.join(log_dir, log_filename),
-    filemode="w",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    encoding="utf-8",
-)
+def set_logger():
+    log_dir = "log"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
-model = BertModel.from_pretrained("bert-base-chinese")
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    log_filename = f"similarity_{current_time}.log"
+    logging.basicConfig(
+        filename=os.path.join(log_dir, log_filename),
+        filemode="w",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        encoding="utf-8",
+    )
 
 
-# Using Mean Pooling
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output.last_hidden_state
     input_mask_expanded = (
@@ -34,44 +31,41 @@ def mean_pooling(model_output, attention_mask):
     sum_mask = input_mask_expanded.sum(1)
     sum_mask = torch.clamp(sum_mask, min=1e-9)
     mean_embeddings = sum_embeddings / sum_mask
-    return mean_embeddings
+    return mean_embeddings.numpy()
 
 
-text = "幫找一些便宜美食的文章"
-encoded_input = tokenizer(
-    text, return_tensors="pt", padding=True, truncation=True, max_length=256
-)
+def find_similar_titles_urls(input_text):
+    set_logger()
+    tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
+    model = BertModel.from_pretrained("bert-base-chinese")
 
-# Generate embeddings for the input text
-with torch.no_grad():
-    model_output = model(**encoded_input)
-    sentence_embedding = mean_pooling(
-        model_output, encoded_input["attention_mask"]
-    ).numpy()
+    encoded_input = tokenizer(
+        input_text, return_tensors="pt", padding=True, truncation=True, max_length=256
+    )
+    with torch.no_grad():
+        model_output = model(**encoded_input)
+        sentence_embedding = mean_pooling(
+            model_output, encoded_input["attention_mask"]
+        ).squeeze(0)
 
-# Load the title to vector mappings
-title_to_vector = torch.load("title_to_vector_1500.pt")
-titles = list(title_to_vector.keys())
-embeddings = torch.stack(list(title_to_vector.values())).squeeze(1).numpy()
+    title_url_to_vector = torch.load("title_url_to_vector_2023_7_1-2024_2_20.pt")
+    titles_urls = list(title_url_to_vector.keys())
+    tensor_list = [
+        torch.tensor(value, dtype=torch.float) for value in title_url_to_vector.values()
+    ]
+    embeddings = torch.stack(tensor_list).numpy()
 
-# Calculate cosine similarity between the input text and each title
-similarities = cosine_similarity(sentence_embedding, embeddings)[0]
+    similarities = cosine_similarity([sentence_embedding], embeddings)[0]
+    top_100_idx = similarities.argsort()[-100:][::-1]
+    top_100_titles_urls = []
 
-# Display similarity score
-for title, similarity in zip(titles, similarities):
-    print(f"Similarity for title '{title}': {similarity}")
-    logging.info(f"Similarity for title '{title}': {similarity}")
+    logging.info("Top 100 most similar titles and URLs:")
+    for rank, idx in enumerate(top_100_idx, start=1):
+        top_title_url = titles_urls[idx]
+        top_similarity = similarities[idx]
+        logging.info(
+            f"Rank {rank}: '{top_title_url}', Similarity score: {top_similarity}"
+        )
+        top_100_titles_urls.append((top_title_url, top_similarity))
 
-top_10_idx = similarities.argsort()[-10:][::-1]
-top_10_titles = []
-
-logging.info("Top 10 most similar titles:")
-for rank, idx in enumerate(top_10_idx, start=1):
-    top_title = titles[idx]
-    top_similarity = similarities[idx]
-    print(f"Rank {rank}: '{top_title}', Similarity score: {top_similarity}")
-    logging.info(f"Rank {rank}: '{top_title}', Similarity score: {top_similarity}")
-    top_10_titles.append(top_title)
-
-
-print("Logger saved.")
+    return top_100_titles_urls
