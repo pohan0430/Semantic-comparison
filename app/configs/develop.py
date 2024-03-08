f04396ca1b1cfc2f4a63e00a7205f060cba41e11
tooooooot <linyugung@gmail.com>
2024-03-08 16:44:19 +0800

    fix: fix the postgres uri of local and container

--- app/configs/develop.py
+++ app/configs/develop.py
@@ -1,3 +1,8 @@
-docker_host_ip = '127.0.0.1'
+# in local connect to postgresql
+# db_host_ip = '127.0.0.1'
+# POSTGRES_URI = f"postgresql+psycopg2://testuser:testpwd@{db_host_ip}:9876/vectordb"
 
-POSTGRES_URI = f"postgresql+psycopg2://testuser:testpwd@{docker_host_ip}:9876/vectordb"
+# in docker container connect to postgresql
+db_host_ip = 'db'
+POSTGRES_URI = f"postgresql+psycopg2://testuser:testpwd@{db_host_ip}:5432/vectordb"
+
