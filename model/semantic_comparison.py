de2c6c5ad8426abd90ad93df6f341a1b249f49f7
pohan <pohan0430@gmail.com>
2024-03-13 16:05:43 +0800

    changing old semantic models to sentence transformer

--- model/semantic_comparison.py
+++ model/semantic_comparison.py
@@ -1,6 +1,8 @@
 import os
 import torch
-from transformers import BertTokenizer, BertModel
+import jieba
+import re
+from sentence_transformers import SentenceTransformer, util
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import logging
@@ -11,7 +13,6 @@ def set_logger():
     log_dir = "log"
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     log_filename = f"similarity_{current_time}.log"
     logging.basicConfig(
@@ -23,63 +24,59 @@ def set_logger():
     )
 
 
-def mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output.last_hidden_state
-    input_mask_expanded = (
-        attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    )
-    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
-    sum_mask = input_mask_expanded.sum(1)
-    sum_mask = torch.clamp(sum_mask, min=1e-9)
-    mean_embeddings = sum_embeddings / sum_mask
-    return mean_embeddings.detach()
+def preprocess_text(text):
+    text = re.sub(r"[^\w\s]", "", text)
+    text = " ".join(jieba.cut(text))
+    return text
+
+
+def get_embedding(input_text):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SentenceTransformer("distiluse-base-multilingual-cased-v2").to(device)
+    # model = SentenceTransformer("paraphrase-multilingual-mpnet-base-v2").to(device)
+    # model = SentenceTransformer("all-mpnet-base-v2").to(device)
+    input_text = preprocess_text(input_text)
+    sentence_embedding = model.encode(input_text, convert_to_tensor=False)
+    return sentence_embedding
 
 
 def find_similar_titles_urls(input_text, top_n_rank=100):
     set_logger()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
-    model = BertModel.from_pretrained("bert-base-chinese").to(device)
-
-    encoded_input = tokenizer(
-        input_text, return_tensors="pt", padding=True, truncation=True, max_length=256
-    ).to(device)
-    with torch.no_grad():
-        model_output = model(**encoded_input)
-        sentence_embedding = (
-            mean_pooling(model_output, encoded_input["attention_mask"])
-            .squeeze(0)
-            .cpu()
-            .numpy()
-        )
-
+    sentence_embedding = get_embedding(input_text)
     abs_path = os.path.abspath(__file__)
     current_dir = os.path.dirname(abs_path)
-    file_path = os.path.join(current_dir, "info_to_vector_2023_7_1-2024_2_20.pt")
+    file_path = os.path.join(current_dir, "DistilBERT.pt")
+    # file_path = os.path.join(current_dir, "mpnet.pt")
     info_to_vector = torch.load(file_path, map_location=torch.device("cpu"))
 
-    similarities = cosine_similarity(
-        [sentence_embedding],
-        np.array([info["vector"] for info in info_to_vector.values()]),
-    )[0]
-    for idx, (_, info) in enumerate(info_to_vector.items()):
-        logging.info(f"Title: {info['info']['title']}, Similarity: {similarities[idx]}")
+    all_news_embeddings = torch.stack(
+        [
+            torch.tensor(info["vector"], dtype=torch.float)
+            for _, info in info_to_vector.items()
+        ]
+    )
 
-    top_indices = np.argsort(similarities)[-top_n_rank:][::-1]
-    top_results = []
+    similarities = util.cos_sim(sentence_embedding, all_news_embeddings).squeeze(0)
 
-    logging.info(f"Top {top_n_rank} most similar titles and URLs:")
-    for rank, idx in enumerate(top_indices, start=1):
-        item = list(info_to_vector.items())[idx]
-        top_info = item[1]["info"]
-        top_similarity = float(similarities[idx])
-        top_info["similarity_score"] = top_similarity
+    for idx, (news_id, info) in enumerate(info_to_vector.items()):
+        similarity_score = similarities[idx].item()
         logging.info(
-            f"Rank: {rank}- Title: {top_info['title']}, Similarity score: {top_similarity}"
+            f"News ID: {news_id}, Title: {info['title']}, Similarity: {similarity_score}"
         )
-        top_results.append(top_info)
 
-    return top_results
+    top_indices = similarities.argsort(descending=True)[:top_n_rank]
+    top_news_ids = [list(info_to_vector.keys())[idx] for idx in top_indices]
+
+    logging.info(f"Top {top_n_rank} most similar titles:")
+    for rank, idx in enumerate(top_indices, start=1):
+        news_id = list(info_to_vector.keys())[idx]
+        info = info_to_vector[news_id]
+        top_similarity = similarities[idx].item()
+        logging.info(
+            f"Rank: {rank} - News ID: {news_id}, Title: {info['title']}, Similarity score: {top_similarity}"
+        )
+
+    return top_news_ids
 
 
-find_similar_titles_urls("找便宜美食", top_n_rank=100)
+top_news_ids = find_similar_titles_urls("提供購買房子建議", top_n_rank=50)
