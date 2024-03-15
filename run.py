0f90bc5d4d5803a2a524375b087f8b56943644f9
pohan <pohan0430@gmail.com>
2024-03-16 02:02:58 +0800

    Rename semanticApp folder to frontend

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from backend import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
